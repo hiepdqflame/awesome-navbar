00491c9cd83784a92614f38240e3528216b676d7
Quang Hiep <bacodekiller@gmail.com>
2019-06-27 10:56:41 +0700

    feat: init project

---
+++ src/components/NavbarFirst.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrap-all">
+    <div class="wrap-content"><h1 class="flame">Navbar</h1></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarFirst"
+};
+</script>
+
+<style lang="scss" scoped>
+.wrap-all {
+  .wrap-content {
+    .flame {
+      color: red;
+    }
+  }
+}
+</style>
+
