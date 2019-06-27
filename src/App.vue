026e4f4f2013b5f7c1d53cf307815ae8753d4f39
Quang Hiep <bacodekiller@gmail.com>
2019-06-27 11:32:29 +0700

    feat: navbar first

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,22 @@
 </template>
 
 <script>
-import NavbarFirst from './components/NavbarFirst.vue'
+import NavbarFirst from "./components/NavbarFirst.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     NavbarFirst
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
